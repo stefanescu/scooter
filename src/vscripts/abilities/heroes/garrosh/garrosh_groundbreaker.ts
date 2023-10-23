762df3722d97eb5a89f2f33d7558a04682288ecf
stefanescu <stefanescuqq@gmail.com>
2023-10-22 21:41:53 -0700

    prep Garr

--- src/vscripts/abilities/heroes/garrosh/garrosh_groundbreaker.ts
+++ src/vscripts/abilities/heroes/garrosh/garrosh_groundbreaker.ts
@@ -3,16 +3,16 @@ import { BaseAbility, registerAbility } from "../../../lib/dota_ts_adapter";
 @registerAbility()
 export class garrosh_groundbreaker extends BaseAbility {
     particle?: ParticleID;
-    cast_anim: GameActivity = GameActivity.DOTA_CAST_ABILITY_4;
-    cast_sound: string = "Hero_Meepo.Earthbind.Cast";
-    texture_name: string = "axe_culling_blade";
-    cast_point: number = 0.4;
+    caster = this.GetCaster();
+    cast_anim = GameActivity.DOTA_CAST_ABILITY_4;
+    cast_sound = "Hero_Meepo.Earthbind.Cast";
+    cast_point = 0.4;
 
     
     GetCooldown() {
         let cooldown = this.GetSpecialValueFor("cooldown");
         if (IsServer()) {
-            const talent = this.GetCaster().FindAbilityByName("special_bonus_unique_meepo_3");
+            const talent = this.caster.FindAbilityByName("special_bonus_unique_meepo_3");
             if (talent) {
                 cooldown -= talent.GetSpecialValueFor("value");
             }
@@ -23,22 +23,18 @@ export class garrosh_groundbreaker extends BaseAbility {
 
     OnAbilityPhaseStart() {
         if (IsServer()) {
-            this.GetCaster().EmitSound(this.cast_sound);
+            this.caster.EmitSound(this.cast_sound);
         }
 
         return true;
     }
 
     OnAbilityPhaseInterrupted() {
-        this.GetCaster().StopSound(this.cast_sound);
+        this.caster.StopSound(this.cast_sound);
     }
 
     GetCastAnimation(): GameActivity {
         return this.cast_anim;
-    }
-
-    GetAbilityTextureName(): string {
-        return this.texture_name;
     }
     
     GetCastPoint(): number {
@@ -46,33 +42,32 @@ export class garrosh_groundbreaker extends BaseAbility {
     }
 
     OnSpellStart() {
-        const caster = this.GetCaster();
         const point = this.GetCursorPosition();
         const projectileSpeed = this.GetSpecialValueFor("speed");
 
-        const direction = ((point - caster.GetAbsOrigin()) as Vector).Normalized();
+        const direction = ((point - this.caster.GetAbsOrigin()) as Vector).Normalized();
         direction.z = 0;
-        const distance = ((point - caster.GetAbsOrigin()) as Vector).Length();
+        const distance = ((point - this.caster.GetAbsOrigin()) as Vector).Length();
 
         const radius = this.GetSpecialValueFor("radius");
         this.particle = ParticleManager.CreateParticle(
             "particles/units/heroes/hero_meepo/meepo_earthbind_projectile_fx.vpcf",
             ParticleAttachment.CUSTOMORIGIN,
-            caster,
+            this.caster,
         );
 
-        ParticleManager.SetParticleControl(this.particle, 0, caster.GetAbsOrigin());
+        ParticleManager.SetParticleControl(this.particle, 0, this.caster.GetAbsOrigin());
         ParticleManager.SetParticleControl(this.particle, 1, point);
         ParticleManager.SetParticleControl(this.particle, 2, Vector(projectileSpeed, 0, 0));
 
         ProjectileManager.CreateLinearProjectile({
             Ability: this,
             EffectName: "",
-            vSpawnOrigin: caster.GetAbsOrigin(),
+            vSpawnOrigin: this.caster.GetAbsOrigin(),
             fDistance: distance,
             fStartRadius: radius,
             fEndRadius: radius,
-            Source: caster,
+            Source: this.caster,
             bHasFrontalCone: false,
             iUnitTargetTeam: UnitTargetTeam.NONE,
             iUnitTargetFlags: UnitTargetFlags.NONE,
@@ -80,17 +75,16 @@ export class garrosh_groundbreaker extends BaseAbility {
             vVelocity: (direction * projectileSpeed) as Vector,
             bProvidesVision: true,
             iVisionRadius: radius,
-            iVisionTeamNumber: caster.GetTeamNumber(),
+            iVisionTeamNumber: this.caster.GetTeamNumber(),
         });
     }
 
     OnProjectileHit(_target: CDOTA_BaseNPC, location: Vector) {
-        const caster = this.GetCaster();
         const duration = this.GetSpecialValueFor("duration");
         const radius = this.GetSpecialValueFor("radius");
 
         const units = FindUnitsInRadius(
-            caster.GetTeamNumber(),
+            this.caster.GetTeamNumber(),
             location,
             undefined,
             radius,
@@ -102,7 +96,7 @@ export class garrosh_groundbreaker extends BaseAbility {
         );
 
         for (const unit of units) {
-            unit.AddNewModifier(caster, this, "modifier_meepo_earthbind", { duration });
+            unit.AddNewModifier(this.caster, this, "modifier_meepo_earthbind", { duration });
             unit.EmitSound("Hero_Meepo.Earthbind.Target");
         }
 
