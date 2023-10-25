b5f7a9eaf0ce3fe7d702a0cb0f595b96ef3dafec
stefanescu <stefanescuqq@gmail.com>
2023-10-24 22:49:43 -0700

    gar decimate

---
+++ src/vscripts/modifiers/garrosh/modifier_decimate_slow.ts
@@ -0,0 +1,17 @@
+import { BaseModifier, registerModifier } from "../../lib/dota_ts_adapter";
+
+// Base speed modifier -- Could be moved to a separate file
+@registerModifier()
+export class modifier_decimate_slow extends BaseModifier {
+
+
+    // Declare functions
+    DeclareFunctions(): ModifierFunction[] {
+        return [ModifierFunction.MOVESPEED_BONUS_PERCENTAGE];
+    }
+
+    GetModifierMoveSpeedBonus_Percentage(): number {
+        return -30;
+    }
+}
+
