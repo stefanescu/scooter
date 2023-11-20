1134a5dcfefa3d95dda7eaab1fd45e930fd4b856
stefanescu <stefanescuqq@gmail.com>
2023-11-20 15:07:14 -0800

    +^

--- src/vscripts/lib/talents.ts
+++ src/vscripts/lib/talents.ts
@@ -4,7 +4,7 @@ import { BaseAbility, BaseModifier, registerAbility, registerModifier } from "./
 
 export const registerTalent = (name?: string, Modifier?: typeof BaseTalentModifier) => (ability: typeof BaseTalent) => {
 	registerAbility(name)(ability);
-    //TODO: write registerAbility(name,ability);
+    //TODO: write function registerAbility(name,ability);
     // I don't know if you can still use decorators like the functions we do here, but at the very least 
     // you should be able to make a separate, non decorator function that does what the decorator does,
     // and call that one instead.
